c1c14826ae16a545aa4ec4844b40a41c0841a123
cloudstudyJ <cloudstudyJ@gmail.com>
2024-03-04 22:36:51 +0900

    easy part implements

--- vector/vec3.h
+++ vector/vec3.h
@@ -5,20 +5,22 @@
 /** TODO LIST
  *      1. cross production return type decision
  *      2. dot production return type decision
+ *      3. support floating points compare
+ *      4. operator +, - type casting decision
 */
 
 template <typename T, typename = enableIF<isArithmetic<T>>>
 class Vec3 {
     public:
         Vec3();
-        Vec3(const Vec3& other);
-        Vec3(Vec3&& other) noexcept;
+        Vec3(const Vec3<T>& other);
+        Vec3(Vec3<T>&& other) noexcept;
         ~Vec3() noexcept;
 
-        template <typename U> Vec3(const U& x, const U& y, const U& z);
+        template <typename U> Vec3(const U& x = { }, const U& y = { }, const U& z = { });
 
-        Vec3& operator=(const Vec3& other);
-        Vec3& operator=(Vec3&& other) noexcept;
+        Vec3& operator=(const Vec3<T>& other);
+        Vec3& operator=(Vec3<T>&& other) noexcept;
 
         template <typename U> Vec3& operator+=(const Vec3<U>& other);
         template <typename U> Vec3& operator-=(const Vec3<U>& other);
@@ -30,6 +32,9 @@ class Vec3 {
         template <typename U> Vec3 operator/(const U& scalar) const;
         template <typename U> Vec3 operator*(const U& scalar) const;
 
+        template <typename U> Vec3& operator()(const U& x, const U& y, const U& z);
+        template <typename U> Vec3& operator()(const Vec3<U>& other);
+
         template <typename U> bool operator==(const Vec3<U>& other) const;
         template <typename U> bool operator!=(const Vec3<U>& other) const;
         template <typename U> bool operator<(const Vec3<U>& other) const;
@@ -37,25 +42,16 @@ class Vec3 {
         template <typename U> bool operator>(const Vec3<U>& other) const;
         template <typename U> bool operator>=(const Vec3<U>& other) const;
 
-        template <typename U> Vec3& operator()(const U& x, const U& y, const U& z);
-        template <typename U> Vec3& operator()(const Vec3<U>& other);
-
         template <typename U> T dot(const Vec3<U>& other) const;
         template <typename U> Vec3 cross(const Vec3<U>& other) const;
 
         template <typename U> void x(const U& x);
         template <typename U> void y(const U& y);
         template <typename U> void z(const U& z);
-        template <typename U> inline void r(const U& x);
-        template <typename U> inline void g(const U& y);
-        template <typename U> inline void b(const U& z);
 
         T x() const noexcept;
         T y() const noexcept;
         T z() const noexcept;
-        inline T r() const noexcept;
-        inline T g() const noexcept;
-        inline T b() const noexcept;
 
         template <typename U> static T dot(const Vec3<T>& v1, const Vec3<U>& v2);
         template <typename U> static Vec3 cross(const Vec3<T>& v1, const Vec3<U>& v2);
@@ -74,3 +70,88 @@ class Vec3 {
             T mB;
         };
 };
+
+template <typename T, typename E> Vec3<T, E>::Vec3() { }
+template <typename T, typename E> Vec3<T, E>::Vec3(const Vec3<T>& other) { *this = other; }
+template <typename T, typename E> Vec3<T, E>::Vec3(Vec3<T>&& other) noexcept { *this = move(other); }
+template <typename T, typename E> Vec3<T, E>::~Vec3() noexcept { }
+
+template <typename T, typename E> template <typename U>
+Vec3<T, E>::Vec3(const U& x, const U& y, const U& z)
+    : mX{static_cast<T>(x)}, mY{static_cast<T>(y)}, mZ{static_cast<T>(z)} { }
+
+template <typename T, typename E>
+Vec3<T, E>& Vec3<T, E>::operator=(const Vec3<T>& other) {
+    mX = other.mX;
+    mY = other.mY;
+    mZ = other.mZ;
+
+    return *this;
+}
+template <typename T, typename E>
+Vec3<T, E>& Vec3<T, E>::operator=(Vec3<T>&& other) noexcept {
+    mX = other.mX;
+    mY = other.mY;
+    mZ = other.mZ;
+
+    other.mX = { };
+    other.mY = { };
+    other.mZ = { };
+
+    return *this;
+}
+
+template <typename T, typename E> template <typename U>
+Vec3<T, E>& Vec3<T, E>::operator/=(const U& scalar) {
+    mX /= scalar;
+    mY /= scalar;
+    mZ /= scalar;
+
+    return *this;
+}
+template <typename T, typename E> template <typename U>
+Vec3<T, E>& Vec3<T, E>::operator*=(const U& scalar) {
+    mX *= scalar;
+    mY *= scalar;
+    mZ *= scalar;
+
+    return *this;
+}
+
+template <typename T, typename E> template <typename U>
+Vec3<T, E> Vec3<T, E>::operator/(const U& scalar) const { return Vec3(mX / scalar, mY / scalar, mZ / scalar); }
+template <typename T, typename E> template <typename U>
+Vec3<T, E> Vec3<T, E>::operator*(const U& scalar) const { return Vec3(mX * scalar, mY * scalar, mZ * scalar); }
+
+template <typename T, typename E> template <typename U>
+Vec3<T, E>& Vec3<T, E>::operator()(const U& x, const U& y, const U& z) {
+    mX = static_cast<T>(x);
+    mY = static_cast<T>(y);
+    mZ = static_cast<T>(z);
+
+    return *this;
+}
+template <typename T, typename E> template <typename U>
+Vec3<T, E>& Vec3<T, E>::operator()(const Vec3<U>& other) {
+    mX = static_cast<T>(other.mX);
+    mY = static_cast<T>(other.mY);
+    mZ = static_cast<T>(other.mZ);
+
+    return *this;
+}
+
+
+
+template <typename T, typename E> template <typename U>
+void Vec3<T, E>::x(const U& x) { mX = static_cast<T>(x); }
+template <typename T, typename E> template <typename U>
+void Vec3<T, E>::y(const U& x) { mY = static_cast<T>(y); }
+template <typename T, typename E> template <typename U>
+void Vec3<T, E>::z(const U& z) { mZ = static_cast<T>(z); }
+
+template <typename T, typename E>
+T Vec3<T, E>::x() const noexcept { return mX; }
+template <typename T, typename E>
+T Vec3<T, E>::y() const noexcept { return mY; }
+template <typename T, typename E>
+T Vec3<T, E>::z() const noexcept { return mZ; }
