a8905b982adaff2955a9a5815c72aa6293b09d6e
cloudstudyJ <cloudstudyJ@gmail.com>
2024-09-10 14:04:41 +0900

    base headerfile

---
+++ base.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "./typeHandler.hpp"
+
+template <typename T, unsigned int DIM, typename = enableIF<isArithmetic<T>>>
+class Vec;
+
+template <typename T, unsigned int ROW, unsigned int COL, typename = enableIF<isArithmetic<T>>>
+class Mat;
